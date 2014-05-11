737651a1276b3d173978ae9b10e61cfcf2722e65
nelo360 <nelo360@gmail.com>
2014-05-11 14:46:59 +0200

    CM11 Themes: Add feature for new themes.
    Change-Id: I0429c576061d8db66c5015226d53464702ab4493

--- config/themes_common.mk
+++ config/themes_common.mk
@@ -5,4 +5,5 @@ PRODUCT_PACKAGES += \
        com.tmobile.themes
 
 PRODUCT_COPY_FILES += \
-       vendor/slim/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+       vendor/slim/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+       vendor/slim/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
